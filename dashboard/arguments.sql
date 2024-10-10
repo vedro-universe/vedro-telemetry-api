ae9315f6edd5fd71d1110e63677a379ecb663271
Nikita Tsvetkov <tsv1@fastmail.com>
2024-10-10 21:10:55 +0400

    remove unused SQL files to clean up the codebase

--- dashboard/arguments.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    name,
-    count(*) as count
-FROM sessions as s
-JOIN arguments as e
-    ON s.id = e.session_id
-WHERE saved_at BETWEEN $__timeFrom() AND $__timeTo()
-GROUP BY name
-ORDER BY count DESC, name ASC
--- dashboard/exceptions.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    type,
-    count(*) as count
-FROM sessions as s
-JOIN exceptions as e
-    ON s.id = e.session_id
-WHERE saved_at BETWEEN $__timeFrom() AND $__timeTo()
-GROUP BY type
-ORDER BY count DESC, type ASC
